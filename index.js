fe064f14191a21655447874cafa50501e51f2890
Bergbok <66174189+Bergbok@users.noreply.github.com>
2025-10-30 11:35:30 +0200

    feat: add Prettier config

---
+++ index.js
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+
+const config = {
+	arrowParens: "always",
+	bracketSpacing: true,
+	endOfLine: "lf",
+	semi: true,
+	singleQuote: true,
+	tabWidth: 4,
+	trailingComma: "none",
+	useTabs: true,
+};
+
+export default config;
