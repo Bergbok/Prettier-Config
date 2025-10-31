46073f43d366445da5b1b7f6e95d50aecbd04c64
Bergbok <66174189+Bergbok@users.noreply.github.com>
2025-10-31 10:49:54 +0200

    feat: override yml indentation width

--- prettier.config.js
+++ prettier.config.js
@@ -8,7 +8,15 @@ const config = {
 	singleQuote: true,
 	tabWidth: 4,
 	trailingComma: 'none',
-	useTabs: true
+	useTabs: true,
+	overrides: [
+		{
+			files: '*.yml',
+			options: {
+				tabWidth: 2
+			}
+		}
+	]
 };
 
 export default config;
